a82f251fd6a14465aba7c79ac9b231141c2f6e38
Artem <gingerman960@gmail.com>
2018-03-07 15:44:02 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', loadChildren: './game/game.module#GameModule'},
+  {path: '**', loadChildren: './game/game.module#GameModule'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
